52760e7abdff7072279b33d0c991b7fb6f6419bd
Shuijuh4 <sebastiansobstad@gmail.com>
2026-05-26 16:37:22 +0200

    Update dunkerScript.js

--- public/js/dunkerScript.js
+++ public/js/dunkerScript.js
@@ -61,7 +61,11 @@ const changeAmount = (sign) => {
     const month = String(today.getMonth() + 1).padStart(2, "0");
     const year = today.getFullYear();
 
-    const formattedDate = `${day}.${month}.${year}`;
+    const hours = String(today.getHours()).padStart(2, "0");
+    const minutes = String(today.getMinutes()).padStart(2, "0");
+    const seconds = String(today.getSeconds()).padStart(2, "0");
+
+    const formattedDate = `${day}.${month}.${year} ${hours}:${minutes}:${seconds}`;
 
     const classIndexRow = classInfo.info.findIndex(entry => entry.Navn == selectedSize);
     
