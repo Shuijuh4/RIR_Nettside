4cafb93dc6cf577c485cac22daef0dc5f3c2899c
Shuijuh4 <sebastiansobstad@gmail.com>
2026-05-26 17:10:41 +0200

    Rename convertToDate function to convertDate

--- public/js/dunkerScript.js
+++ public/js/dunkerScript.js
@@ -237,7 +237,7 @@ const logEvent = (logButton) => {
         const c1 = row.insertCell(0);
         const c2 = row.insertCell(1);
     
-        const date = convertToDate(logInfo[i][0]);
+        const date = convertDate(logInfo[i][0]);
     
         const day = String(date.getDate()).padStart(2, "0");
         const month = String(date.getMonth() + 1).padStart(2, "0");
